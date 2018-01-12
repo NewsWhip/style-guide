49bb5e8f2ebbf7d84f0ff3361ba45144eb106621
Gareth Nolan <garethdn@gmail.com>
2018-01-12 22:58:20 +0000

    prettify code blocks

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var html_beautify: any;
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html'
+    selector: 'app-list',
+    templateUrl: './list.component.html'
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+    @ViewChild('lg') lg: ElementRef;
+    @ViewChild('lgPrimary') lgPrimary: ElementRef;
 
-  ngOnInit() {
-  }
+    public listGroupHTML: string;
+    public listGroupPrimaryHTML: string;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        console.log(this.lg);
+
+        setTimeout(() => {
+            this.listGroupHTML = html_beautify(this.lg.nativeElement.outerHTML);
+            this.listGroupPrimaryHTML = html_beautify(this.lgPrimary.nativeElement.outerHTML);
+        }, 0);
+    }
+
+    escapeHtml(unsafe: string) {
+        return unsafe
+            .trim()
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
 
 }
