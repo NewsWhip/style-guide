e54491ef33c9cedbaed2071ba0713485a9f9f0dd
conorjohn <conorj92@gmail.com>
2018-04-27 16:09:01 +0100

    update: html, css and js on the sliding animation for tabs

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -13,12 +13,29 @@ export class TabsComponent implements OnInit {
     'Messages',
     'Settings'
   ];
+  public navTabsSecondary = [
+      'Content 1',
+      'Con rewgwre tent 2',
+      'Content 3',
+      'Con rg qwe tent 4',
+      'Con rg qwe te qewfqwf efq eqwf'
+  ];
+  public secondarySelectedTab = 'Content 1';
 
-  public selectedTab = "Home";
+  public selectedTab = 'Home';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  animateLeftPos(): number {
+      const elm: any = document.getElementById('tabs-control').getElementsByClassName('active')[0].getBoundingClientRect();
+      return elm.left;
+  }
+
+  setWidth(): number {
+      const elm: any = document.getElementById('tabs-control').getElementsByClassName('active')[0].getBoundingClientRect();
+      return elm.width;
+  }
 }
