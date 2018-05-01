df4952f5cd5d04c73daa0ee9fe5d3b903dd5db8d
codeepic <ksaweryglab@gmail.com>
2018-05-01 10:34:07 +0100

    style: popover

--- src/_lib/modules/feature-alerts/feature-alerts.module.ts
+++ src/_lib/modules/feature-alerts/feature-alerts.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FeatureAlertComponent } from './feature-alert.component';
 import { FeatureAlertsService } from './feature-alerts.service';
 import { WindowRef } from './windowref';
-import {PopoverModule} from "ngx-bootstrap";
+import {PopoverModule} from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    PopoverModule.forRoot() //thanks to this line we no longer have to
+    PopoverModule.forRoot() // thanks to this line we no longer have to
     // import it in app.module.ts ofthe demo app
   ],
   providers: [WindowRef],
