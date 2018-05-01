d8ec55203cdfa214d062afec13de6b1e7f46cdd8
codeepic <ksaweryglab@gmail.com>
2018-05-01 13:21:05 +0100

    test: fix more boilerplate tests

--- src/app/buttons/buttons.component.spec.ts
+++ src/app/buttons/buttons.component.spec.ts
@@ -1,25 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonsComponent } from './buttons.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('ButtonsComponent', () => {
-  let component: ButtonsComponent;
-  let fixture: ComponentFixture<ButtonsComponent>;
+    let component: ButtonsComponent;
+    let fixture: ComponentFixture<ButtonsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ButtonsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ButtonsComponent ],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ButtonsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
