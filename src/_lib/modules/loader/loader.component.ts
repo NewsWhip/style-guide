80d7099e455e84862f5abc89beb80275c21a7447
conorjohn <conorj92@gmail.com>
2019-01-21 18:35:05 +0000

    chore: small scss changes

--- src/_lib/modules/loader/loader.component.ts
+++ src/_lib/modules/loader/loader.component.ts
@@ -3,14 +3,14 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'nw-loader',
     template: `
-      <div *ngIf="isLoading;" class="nw-loader {{size}}">
+      <div *ngIf="isLoading;" class="loader {{size}}">
         <div class="dots-icon-wrapper">
           <div class="dots">
             <span *ngFor="let d of dots" class="dot dot-{{d}}"></span>
           </div>
         </div>
       </div>
-  `
+    `
 })
 
 export class LoaderComponent {
@@ -18,9 +18,7 @@ export class LoaderComponent {
   @Input() numOfDots: number = 3;
   @Input() size?: 'sm' | 'md' | 'lg' = 'md';
 
-
   get dots(): number[] {
-    console.log(this.numOfDots)
     return Array(this.numOfDots).fill(0).map((x, i) => i + 1);
   }
 }
