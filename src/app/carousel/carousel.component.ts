06bf4684b29b0c7cc8ad5ae4532bea4996d4f3df
Gareth Nolan <garethdn@gmail.com>
2019-02-04 10:30:39 +0000

    clean up examples, generate docs, remove log method

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-carousel',
@@ -9,7 +10,7 @@ export class CarouselComponent implements OnInit {
 
     public items: any[] = [];
 
-    constructor() { }
+    constructor(private _domSanitizer: DomSanitizer) { }
 
     ngOnInit() {
         this.items = this.getItems();
@@ -19,6 +20,37 @@ export class CarouselComponent implements OnInit {
         return [...Array(30).fill(1)].map((_, i) => i);
     }
 
-    log = console.log
+    getExample1() {
+        return `<nw-carousel maskColor="#373737">
+    <ng-container *ngFor="let item of items">
+        <div class="slide" nwCarouselSlide snapAlign="center" (click)="log('test')"
+            [style.background-image]="'url(https://picsum.photos/216/120?image=' + item +')'">Slide {{item}}</div>
+    </ng-container>
+</nw-carousel>`
+    }
+
+    getExample2() {
+        return `<nw-carousel maskColor="#373737" #carousel="nw-carousel" [showPageIndicator]="false" [showPagination]="false">
+    <ng-container *ngFor="let item of items">
+        <div class="slide"
+            [style.background-color]="'grey'">Slide {{item}}</div>
+    </ng-container>
+
+    <div class="pagination-left">
+        <button class="btn btn-primary btn-md" (click)="carousel.prev()">Prev</button>
+    </div>
+
+    <div class="pagination-right">
+        <button class="btn btn-primary btn-md" (click)="carousel.next()">Next</button>
+    </div>
+
+    <div class="pagination-indicators" style="width: 100%; text-align: center;">
+        <button class="btn sm btn-primary" *ngFor="let page of carousel.getPages()" [class.active]="page === carousel.currPage"
+            (click)="carousel.goToPage(page)">
+            {{page + 1}}
+        </button>
+    </div>
+</nw-carousel>`
+    }
 
 }
