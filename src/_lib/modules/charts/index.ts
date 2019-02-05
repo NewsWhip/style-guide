1c155611ebd49795d0a9626c10e4840e2d728703
Gareth Nolan <garethdn@gmail.com>
2019-02-05 10:04:46 +0000

    update module to be built

---
+++ src/_lib/modules/charts/index.ts
@@ -0,0 +1,8 @@
+export { ChartsModule } from './charts.module';
+export { ChartComponent } from './chart.component';
+export { PathDirective } from './path/path.directive';
+export { XAxisDirective } from './axis/x-axis.directive';
+export { YAxisDirective } from './axis/y-axis.directive';
+export { CircleDirective } from './circle/circle.directive';
+export { BrushDirective } from './brush/brush.directive';
+export { BarDirective } from './bar/bar.directive';
