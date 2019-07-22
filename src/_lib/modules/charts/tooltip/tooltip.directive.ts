a8483fa7b422b624335bbec4058828e479285929
Dina Malone <dina.malone@newswhip.com>
2019-07-22 16:09:38 +0100

    flip position

--- src/_lib/modules/charts/tooltip/tooltip.directive.ts
+++ src/_lib/modules/charts/tooltip/tooltip.directive.ts
@@ -1,21 +1,40 @@
-import {Directive, ElementRef, Input, Renderer2, SimpleChanges} from '@angular/core';
+import {Directive, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges} from '@angular/core';
 
 @Directive({
     selector: '[nwChartTooltip]'
 })
-export class TooltipDirective {
+export class TooltipDirective implements OnChanges, OnInit {
     @Input() position: [number, number];
+    @Input() chartWidth: number;
+    @Input() chartHeight: number;
+    @Input() chartMargins: {
+        top?: number;
+        bottom?: number;
+        left?: number;
+        right?: number;
+    };
+    private _width: number;
+    private _height: number;
 
     constructor(
         private _elRef: ElementRef,
         private _renderer: Renderer2) {
     }
 
+    ngOnInit() {
+        this._width = this._elRef.nativeElement.getBoundingClientRect().width;
+        this._height = this._elRef.nativeElement.getBoundingClientRect().height;
+    }
+
     ngOnChanges(changes: SimpleChanges) {
         if (changes.position && changes.position.previousValue !== changes.position.currentValue) {
-            this._renderer.setStyle(
-                this._elRef.nativeElement, 'transform',
-                `translate(calc(50% + ${changes.position.currentValue[0]}px), calc(50% + ${changes.position.currentValue[1]}px))`);
+            const offsetLeft = changes.position.currentValue[0] + this.chartMargins.left;
+            const offsetTop = changes.position.currentValue[1] + this.chartMargins.top;
+
+            const x: string = offsetLeft > this.chartWidth - this._width ? `calc(${offsetLeft - 8}px - 100%)` : `${offsetLeft + 8}px`;
+            const y: string = offsetTop > this.chartHeight - this._height ? `calc(${offsetTop}px - 100%)` : `${offsetTop}px`;
+
+            this._renderer.setStyle(this._elRef.nativeElement, 'transform', `translate(${x}, ${y})`);
         }
     }
 }
