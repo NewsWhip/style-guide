f3673e69f35e73304a47e128ae5057870d47e2cd
Dina Malone <dina.malone@newswhip.com>
2019-07-23 15:11:22 +0100

    fix: prevent flicker

--- src/_lib/modules/charts/tooltip/chart-tooltip.directive.ts
+++ src/_lib/modules/charts/tooltip/chart-tooltip.directive.ts
@@ -23,22 +23,37 @@ export class ChartTooltipDirective implements OnChanges, OnInit {
     }
 
     ngOnInit() {
-        const rect = this._elRef.nativeElement.getBoundingClientRect();
-        this._width = rect.width;
-        this._height = rect.height;
+        this._setTooltipSize();
+
+        this._renderer.setStyle(this._elRef.nativeElement, 'pointer-events', 'none');
     }
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes.position && changes.position.previousValue !== changes.position.currentValue) {
+            // Setting the tooltip size here, as well as in ngOnInit, prevents this._width & this._height from being undefined
+            // as a result of the first ngOnChanges being called before ngOnInit.
+            // The contents of this method are only executed if this._width or this._height are undefined, which prevents
+            // doing the same work twice, and (hopefully) makes this operation a bit less expensive.
+            this._setTooltipSize();
+
             const offsetLeft = changes.position.currentValue[0] + this.chartMargins.left;
             const offsetTop = changes.position.currentValue[1] + this.chartMargins.top;
 
-            const x: string = offsetLeft > this.chartWidth + this.chartMargins.left + this.chartMargins.right - this._width ?
-                `calc(${offsetLeft - this.offsetX}px - 100%)` : `${offsetLeft + this.offsetX}px`;
-            const y: string = offsetTop > this.chartHeight + this.chartMargins.top + this.chartMargins.bottom - this._height ?
-                `calc(${offsetTop}px - 100%)` : `${offsetTop}px`;
+            const isFlippedX = offsetLeft > this.chartWidth + this.chartMargins.left + this.chartMargins.right - this.offsetX - this._width;
+            const isFlippedY = offsetTop > this.chartHeight + this.chartMargins.top + this.chartMargins.bottom - this._height;
+
+            const x: string = isFlippedX ? `calc(${offsetLeft - this.offsetX}px - 100%)` : `${offsetLeft + this.offsetX}px`;
+            const y: string = isFlippedY ? `calc(${offsetTop}px - 100%)` : `${offsetTop}px`;
 
             this._renderer.setStyle(this._elRef.nativeElement, 'transform', `translate(${x}, ${y})`);
+        }
+    }
+
+    private _setTooltipSize(): void {
+        if (this._width === undefined || this._height === undefined) {
+            const rect = this._elRef.nativeElement.getBoundingClientRect();
+            this._width = rect.width;
+            this._height = rect.height;
         }
     }
 }
