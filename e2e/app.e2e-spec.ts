e7daf2168318196d5fad26b8b87c31c1d8220377
Gareth Nolan <garethdn@gmail.com>
2020-11-05 12:22:24 +0000

    begin cleanup of angular.json by renaming the project for the demo app

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('nwstyle-guide App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('NewsWhip Style Guide');
   });
 });
