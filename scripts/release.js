ee05f09fa8e7aef8c733177e00aa7230e326c563
Gareth Nolan <garethdn@gmail.com>
2021-11-25 10:45:56 +0000

    feat: add docs for 2fa and update release script to capture child stdio

--- scripts/release.js
+++ scripts/release.js
@@ -8,7 +8,16 @@ const run = () => {
     process.stdout.write('Publishing package \n');
 
     try {
-        execSync(`npm publish ${utils.distPath} --dry-run`);
+        /**
+         * We want the child process executed here to use the parent std in and out so that we can enter
+         * the one-time-password (OTP) that we should be prompted for due the 2FA applied to the package
+         *
+         * ref: https://nodejs.org/api/child_process.html#optionsstdio
+         * ref: https://stackoverflow.com/a/31104898/1128290
+         */
+        execSync(`npm publish ${utils.distPath}`, {
+            stdio: 'inherit'
+        });
     } catch(err) {
         throw err;
     }
