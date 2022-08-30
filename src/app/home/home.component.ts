7e51cd4358370c5acf782ef9266a54c9dba9c562
Gareth Nolan <garethdn@gmail.com>
2022-08-30 11:13:12 +0100

    chore: remove ngx-md dependency

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxMdService } from 'ngx-md';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private _markdown: NgxMdService) { }
-
-  ngOnInit() {
-    this._markdown.renderer.heading = (text: string, level: number) => {
-      return `<h${level} ${level === 1 ? 'class="page-header"' : ''}>${text}</h${level}>`;
-    };
-  }
+  public version = VERSION;
 
 }
