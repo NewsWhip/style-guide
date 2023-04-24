0a0b1dace014a01a0f42c24b5416c0515d95060e
andrew-j-byrne <andrew.byrne@newswhip.com>
2023-04-24 17:25:44 +0100

    APP-240 added better code snippets

--- src/app/spacing/spacing.component.ts
+++ src/app/spacing/spacing.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ISnippet } from '../code/ISnippet';
 
 @Component({
   selector: 'app-spacing',
@@ -6,6 +7,66 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SpacingComponent implements OnInit {
+
+  public snippets: ISnippet[] = [
+    {
+      lang: 'css',
+      code: `
+        .ml-4 {
+          margin-left: 4px;
+        }
+      `
+    },
+    {
+      lang: 'css',
+      code: `
+      .mt-16 {
+        margin-top: 16px;
+      }
+      `
+    },
+    {
+      lang: 'css',
+      code: `
+      .mx-4 {
+        margin-left: 4px;
+        margin-right: 4px;
+      }
+      `
+    },
+    {
+      lang: 'css',
+      code: `
+      .mr-auto {
+        margin-right: auto;
+      }
+      `
+    },
+    {
+      lang: 'css',
+      code: `
+      .p-8 {
+        padding: 8px;
+      }
+      `
+    }
+  ];
+
+  public usageSnippets: ISnippet[] = [
+    {
+      lang: 'html',
+      code: '<div class="mr-8">...</div>'
+    },
+    {
+      lang: 'html',
+      code: '<p class="my-16">...</p>'
+    },
+    {
+      lang: 'html',
+      code: '<ul class="pl-0">...</ul>'
+    },
+    
+  ]
 
   constructor() { }
 
