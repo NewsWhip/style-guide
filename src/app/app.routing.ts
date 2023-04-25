8a8ed4739dad001ed695af6bb351680fb29151e3
Gareth Nolan <garethdn@gmail.com>
2023-04-25 09:44:17 +0100

    chore: automatic upgrade steps

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -139,4 +139,4 @@ const appRoutes: Routes = [
     }
 ];
 
-export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes, { useHash: true, relativeLinkResolution: 'legacy' });
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes, { useHash: true });
