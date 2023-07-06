1661149fb50035606883f33fba2fe52064a6dc3e
Gareth Nolan <garethdn@gmail.com>
2023-07-06 17:06:21 +0100

    use providedIn instead of forRoot and use provideRouter

--- src/_lib/modules/feature-alerts/index.ts
+++ src/_lib/modules/feature-alerts/index.ts
@@ -1,4 +1,3 @@
-export { FeatureAlertsModule } from './feature-alerts.module';
 export { FeatureAlertsService } from './feature-alerts.service';
 export { IFeatureAlertParams } from './IFeatureAlertParams';
 export { HotspotComponent } from './hotspot.component';
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,12 @@ import { AppComponent } from './app/app.component';
 import { DialogModule } from '@angular/cdk/dialog';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { PortalModule } from '@angular/cdk/portal';
-import { ToastsModule } from './_lib/modules/toasts';
-import { FeatureAlertsModule } from './_lib/modules/feature-alerts';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { routing } from './app/app.routing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routing';
 
 if (environment.production) {
     enableProdMode();
@@ -18,8 +17,9 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule, routing, FeatureAlertsModule, ToastsModule.forRoot(), FeatureAlertsModule.forRoot(), PortalModule, ScrollingModule, DialogModule),
+        importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule, PortalModule, ScrollingModule, DialogModule),
         provideAnimations(),
+        provideRouter(routes, withHashLocation()),
         provideHttpClient(withInterceptorsFromDi())
     ]
 }).catch(err => console.log(err));
