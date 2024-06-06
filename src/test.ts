ecc04aacb1d8b7cde17595c162501925993b9301
Walid Kurchied <walid.kurchied@newswhip.com>
2024-06-06 14:42:50 +0100

    15.0.0-beta.0

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,4 @@
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
