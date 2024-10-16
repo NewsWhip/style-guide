5c07ea49f08c1a1c79a56dfd7d7160913fd2a9ae
Gareth Nolan <garethdn@gmail.com>
2024-10-16 16:26:28 +0100

    debug

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROMIUM_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -34,10 +35,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['HeadlessChrome'],
       customLaunchers: {
           HeadlessChrome: {
-              base: 'ChromeHeadless',
+              base: 'ChromiumHeadless',
               flags: [
                   '--no-sandbox',
                   '--headless=new'
