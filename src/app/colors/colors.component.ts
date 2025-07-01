f5aef6f45989dd17d4373c13fc8929de2dea781c
Gareth Nolan <garethdn@gmail.com>
2025-07-01 15:40:45 +0100

    refactor

--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'app-colors',
-    templateUrl: './colors.component.html'
+    templateUrl: './colors.component.html',
+    styleUrls: ['./colors.component.scss']
 })
 export class ColorsComponent implements OnInit {
     public showColorCodes: boolean = false;
--- src/app/dropdowns/dropdowns.component.ts
+++ src/app/dropdowns/dropdowns.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-dropdowns',
-    templateUrl: './dropdowns.component.html'
+    templateUrl: './dropdowns.component.html',
+    styleUrls: ['./dropdowns.component.scss']
 })
 export class DropdownsComponent implements OnInit {
 
