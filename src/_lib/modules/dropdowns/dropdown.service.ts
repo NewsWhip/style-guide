35ff9f401ffb2682f0628e777368793c49613aa2
Gareth Nolan <garethdn@gmail.com>
2025-07-07 15:52:47 +0100

    remove unused import

--- src/_lib/modules/dropdowns/dropdown.service.ts
+++ src/_lib/modules/dropdowns/dropdown.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, Subject, skip } from "rxjs";
+import { BehaviorSubject, Observable, skip } from "rxjs";
 
 @Injectable()
 export class DropdownService {
