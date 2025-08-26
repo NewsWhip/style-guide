abef68fb82eccafb23420ae2c354d64d0617b8cd
Gareth Nolan <garethdn@gmail.com>
2025-08-26 16:34:21 +0100

    remove old assets location and update refs

--- projects/nw-style-guide/src/public-api.ts
+++ projects/nw-style-guide/src/public-api.ts
@@ -1,18 +1,7 @@
-/*
+/**
  * Public API Surface of style-guide
+ *
+ * Although all our entry points are secondary entry points, this file is required
+ * by ng-packagr to build the library.
  */
 export {};
-// export * from './lib/animations';
-// export * from './lib/autofocus';
-// export * from './lib/carousel';
-// export * from './lib/charts';
-// export * from './lib/dropdowns';
-// export * from './lib/email-input';
-// export * from './lib/feature-alerts';
-// export * from './lib/loader';
-// export * from './lib/picker';
-// export * from './lib/resize-observer';
-// export * from './lib/tabs';
-// export * from './lib/toasts';
-// export * from './lib/tooltips';
-// export * from './lib/word-cloud';
