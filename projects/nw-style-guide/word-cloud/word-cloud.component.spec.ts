8b7c928c6124cfb06119bb44a96df304b79b0794
Gareth Nolan <garethdn@gmail.com>
2025-08-27 09:51:34 +0100

    in test spec, don't import from dist

--- projects/nw-style-guide/word-cloud/word-cloud.component.spec.ts
+++ projects/nw-style-guide/word-cloud/word-cloud.component.spec.ts
@@ -1,6 +1,6 @@
 import { DebugElement, SimpleChange } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { ResizeObserverModule } from "nw-style-guide/resize-observer";
+import { ResizeObserverModule } from "../resize-observer";
 import { IBoundingBox } from "./models/IBoundingBox";
 import { IWord } from "./models/IWord";
 import { IWordWithPosition } from "./models/IWordWithPosition";
