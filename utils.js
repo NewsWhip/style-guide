961d796592776a490d03adc6a67607474b644fd2
Gareth Nolan <garethdn@gmail.com>
2025-08-28 11:33:55 +0100

    change library output dist folder so that demo-app can watch it for changes

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const green = '\033[0;92m';
 const red = '\033[0;91m';
 const noColor = '\033[0m';
 
-exports.distPath = 'dist/nw-style-guide/';
+exports.distPath = 'lib-dist/nw-style-guide/';
 exports.green = green;
 exports.red = red;
 exports.noColor = noColor;
