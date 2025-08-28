8ce30fdc078b88483716db26bf2f2b2961778c96
Gareth Nolan <garethdn@gmail.com>
2025-08-28 12:21:17 +0100

    simplify things by removing the mostly redundant index.ts files

--- projects/nw-style-guide/tabs/tabs.service.ts
+++ projects/nw-style-guide/tabs/tabs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Subject, Observable } from "rxjs";
-import { TabDirective } from ".";
+import { TabDirective } from "./tab.directive";
 
 @Injectable()
 export class TabsService {
